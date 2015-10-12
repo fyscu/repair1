1e8a70aa23fb03832a8bdfaed33d63247cd523f1
dsgygb <dsgygb@126.com>
2015-10-13 01:09:06 +0800

    for init

---
+++ Application/Common/Conf/config.example.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+	'STOP_REPAIR'=>0, //报修系统是否关闭
+	// 'SUPER_URL'=>'http://uc.fyscu.com/superlogin',
+ //    'UC_LOGIN_URL'=>"http://uc.fyscu.com?appid=1009",
+	// 'UC_API'=>"http://uc.fyscu.com/api",
+	'SUPER_URL'=>'http://http://121.41.85.236:9527/superlogin',
+    'UC_LOGIN_URL'=>"http://http://121.41.85.236:9527?appid=1009",
+	'UC_API'=>"http://http://121.41.85.236:9527/api",
+	"APP_ID"=>1009,
+	"APP_KEY"=>"xxxxxxxxx",
+	'URL_HTML_SUFFIX'       =>  '',  // URL伪静态后缀设置
+	'URL_MODEL'=>'2',
+	'URL_CASE_INSENSITIVE' =>false,
+	'TMPL_FILE_DEPR'=>'_',
+	'DEFAULT_MODULE'=>'Home',
+	'TMPL_VAR_IDENTIFY'=>'array',
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'dbname',//数据库名
+	'DB_USER'=>'user',//数据库用户名
+	'DB_PWD'=>'123456',//数据库密码
+	'DB_PORT'=>3306,
+	'DB_PREFIX'=>'fy_',
+	'DEFAULT_THEME'=>'Simple',
+	'TMPL_L_DELIM'=>'@#',
+	'TMPL_R_DELIM'=>'#@',
+
+);
+
+?>
---
+++ Application/Home/Controller/AccountPageController.class.php
@@ -0,0 +1,123 @@
+<?php
+	namespace Home\Controller;
+	use Think\Controller;
+
+	class AccountPageController extends Controller{
+
+		public function logout(){
+			session(null);
+			$this->success('退出成功！',C('UC_LOGIN_URL'));
+		}
+
+		public function ucLogin(){
+
+			if(!$_GET['access_token']){
+				$this->error('没有token,请重新登录',C('UC_LOGIN_URL'));
+				exit;
+			}
+			$userinfo= json_decode(curl('http://uc.fyscu.com/api/getuserinfo?appid=1009&appkey=533e55482f4d07df&token='.$_GET['access_token']),1);
+
+			//dump($userinfo);exit;
+			if($userinfo['code']!=0){
+				$this->error('获取资料失败,请重新登录'.$userinfo['msg'],C('UC_LOGIN_URL'));
+				exit;
+			}
+
+			session('ucid',$userinfo['data'][0]['uid']);
+			session('access_token',$_GET['access_token']);
+			$a=is_user($userinfo['data'][0]['uid'],'ucid');
+			$telUser=is_userExtend($userinfo['data'][0]['tel']);
+			if($a){
+				//dump($a);exit;
+				$user_id=$a['user_id'];
+				$type=$a['type'];
+				session('user_id',$user_id);
+				session('type',$type);
+				if($_SESSION['type']==3){
+					$b=M('admin');
+					$admin_map['user_id']=$user_id;
+					$c=$b->where($admin_map)->find();
+					session('admin_id',$c['admin_id']);
+					redirect('/Home/AdminPage/index?access_token='.$_GET['access_token']);
+				}elseif($_SESSION['type']==2){
+					$b=M('staff');
+					$staff_map['user_id']=$user_id;
+					$c=$b->where($staff_map)->find();
+					//dump($c);exit;
+					session('staff_id',$c['staff_id']);
+					redirect('/Home/StaffPage/not?access_token='.$_GET['access_token']);
+
+				}else{
+					redirect('/Home/Index/index?access_token='.$_GET['access_token']);
+				}
+			}else if($telUser){
+				$user=M('user');
+				$usermap['user_id']=$telUser['user_id'];
+				$data['ucid']=$userinfo['data'][0]['uid'];
+				$user->where($usermap)->save($data);
+				$user_id=$telUser['user_id'];
+				$type=$user->where($usermap)->getField('type');
+
+
+				session('user_id',$user_id);
+				session('type',$type);
+				if($_SESSION['type']==3){
+					$b=M('admin');
+					$admin_map['user_id']=$user_id;
+					$c=$b->where($admin_map)->find();
+					session('admin_id',$c['admin_id']);
+					redirect('/Home/AdminPage/index?access_token='.$_GET['access_token']);
+				}elseif($_SESSION['type']==2){
+					//echo '2';exit;
+					$b=M('staff');
+					$staff_map['user_id']=$user_id;
+					$c=$b->where($staff_map)->find();
+					//dump($c);exit;
+					session('staff_id',$c['staff_id']);
+					redirect('/Home/StaffPage/not?access_token='.$_GET['access_token']);
+
+				}else{
+					//dump($_SESSION);exit;
+					redirect('/Home/Index/index?access_token='.$_GET['access_token']);
+				}
+			}else{
+
+				session('tel',$userinfo['data'][0]['tel']);
+				redirect('/Home/AccountPage/register?access_token='.$_GET['access_token']);
+			}
+
+		}
+		public function register(){
+
+
+			if (C('STOP_REPAIR')) {
+				$this->error('您好,飞扬报修系统由于一些原因暂时关闭系统。系统重新开放后，我们将会在四川大学飞扬俱乐部官方微信/微博进行通知，尽请留意！', '', 8);
+				exit;
+			}
+
+			if($_GET['access_token']){
+				$uid=is_tokenLogin($_GET['access_token']);
+				if(!$uid){
+					$this->error('登录超时,请重新登录',C('UC_LOGIN_URL'));
+				}else{
+
+					if(is_userExtend($_SESSION['tel'])){
+						$this->error('您已经注册过飞扬报修系统了,将跳转至首页','/Home/Index/index?access_token='.$_GET['access_token']);
+						exit;
+					}else{
+						$this->display();
+					}
+				}
+			}else if($_SESSION['access_token']){
+					redirect(__SELF__.'?access_token='.$_SESSION['access_token']);
+			} else {
+				not_login();
+			}
+
+
+
+		}
+		
+
+	}
+?>
