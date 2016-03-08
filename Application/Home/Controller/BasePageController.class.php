4479b365b32d1beb929b9edaa0a7c3f4962556e8
captainblue2013 <admin@lanhao.name>
2016-03-08 10:55:59 +0800

    format

--- Application/Home/Controller/BasePageController.class.php
+++ Application/Home/Controller/BasePageController.class.php
@@ -7,9 +7,9 @@ class BasePageController extends Controller
 {
 
     private $_fyuc = null;
+
     /*------初始化...------*/
-    public function _initialize()
-    {
+    public function _initialize() {
         if (C('STOP_REPAIR')) {
             $this->error('您好,由于临近期中考试，飞扬报修系统暂时关闭系统。系统重新开放后，我们将会在四川大学飞扬俱乐部官方微信/微博进行通知，尽请留意！', '', 8);
             exit;
@@ -21,7 +21,7 @@ class BasePageController extends Controller
             if ($_GET['token'] != $_SESSION['token']) {
                 session(null);
                 session('token', $_GET['token']);
-                redirect('/Home/AccountPage/ucLogin?token=' . $_GET['token'].'&account='.$_GET['account']);
+                redirect('/Home/AccountPage/ucLogin?token=' . $_GET['token'] . '&account=' . $_GET['account']);
                 exit;
             }
 
@@ -34,14 +34,14 @@ class BasePageController extends Controller
         } else {
 
 
-              if(C('DEBUG')){
-session('user_id',37);
-            session('type',1);
+            if (C('DEBUG')) {
+                session('user_id', 37);
+                session('type', 1);
 
-              }else{
-                        not_login();
+            } else {
+                not_login();
 
-              }
+            }
         }
     }
 }
